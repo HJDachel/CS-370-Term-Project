bb47c97a540739f05891c860214dc27c6b99c446
hjdachel <hjdachel@outlook.com>
2020-12-05 18:33:32 -0700

    start writing code for DB

--- server.js
+++ server.js
@@ -10,13 +10,33 @@
  */
 
 /** requires */
+const e = require('express');
 const express = require('express'); // require express for web server and for get, post methods, etc.
 /** instantiate requires and name them */
 const server = express();
 
+const mysql = require('mysql');
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.text());
+
+var mysqlConnection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root', 
+    password: 'password',
+    database: 'dates',
+    multipleStatements: true
+});
+
+mysqlConnection.connect((err)=> {
+    if (!err) {
+        console.log("Successfully connected to DB");
+    }else {
+        console.log("Connection to DB failed" + JSON.stringify(err, undefined, 2));
+    }
+});
+
 
 /** method to test express */
 server.get("/test", (req, res) => {
@@ -32,9 +52,17 @@ server.get("/front", (req, res) => {
 
 /** REST methods. For now just returns request body */
 server.get("/api", (req, res) =>  {
-    console.log("GET request");
-    console.log(req.body);
-    res.send(req.body);
+    // console.log("GET request");
+    // console.log(req.body);
+    // res.send(req.body);
+    console.log(req.body.date);
+    mysqlConnection.query('SELECT * FROM dates WHERE date=?', [req.body.date], (err, rows, fields) => {
+        if (!err) {
+            res.send(rows);
+        }else {
+            console.log(err);
+        }
+    });
 });
 
 server.post("/api", (req, res) => {
